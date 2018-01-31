b7d4e40d291b663c35e5b9985fe96f24dee83dcd
her2814 <her2814@naver.com>
2018-01-31 18:01:32 +0900

    git push

--- client/lib/helper.js
+++ client/lib/helper.js
@@ -2,3 +2,7 @@ Template.registerHelper("currentMode",()=>{
     if(!Session.get("viewMode")) Session.set("viewMode","chatMain");
     return Session.get("viewMode");
 });
+
+Template.registerHelper("ago",(timestamp)=>{
+    return moment(timestamp).fromNow();
+})
